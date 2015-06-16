bd6db2e9436a9594c199380a42acb867a8653d29
FredGoo <545976148@qq.com>
2015-06-16 15:22:40 +0800

    form

--- resources/views/applyZan.blade.php
+++ resources/views/applyZan.blade.php
@@ -6,9 +6,31 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
-
 				<div class="panel-body">
-          我要申请一个赞
+          <!-- apply form -->
+          <form>
+            <div class="form-group">
+              <label>标题</label>
+              <input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+              <label>个数</label>
+              <select class="form-control">
+                <option value="1">1</option>
+                <option value="1">2</option>
+                <option value="1">3</option>
+                <option value="1">5</option>
+                <option value="1">10</option>
+                <option value="1">15</option>
+                <option value="1">30</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>内容</label>
+              <textarea class="form-control"></textarea>
+            </div>
+          </form>
+          <!-- ./ apply form -->
 				</div>
 			</div>
 		</div>
