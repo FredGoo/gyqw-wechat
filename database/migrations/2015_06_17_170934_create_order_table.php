58d37be84604c13d52f0f852911d0b2c20acac31
FredGoo <545976148@qq.com>
2015-06-17 17:14:57 +0800

    add table order

---
+++ database/migrations/2015_06_17_170934_create_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('order', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('from_user');
+			$table->integer('to_user');
+			$table->string('title');
+			$table->string('content');
+			$table->integer('num');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('order');
+	}
+
+}
