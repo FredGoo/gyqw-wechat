672eca11cab9c57589206eb9b933a0ae8099445b
FredGoo <545976148@qq.com>
2015-06-17 16:16:56 +0800

    create user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name');
+			$table->string('wechat_open_id');
 			$table->string('email')->unique();
 			$table->string('password', 60);
 			$table->rememberToken();
