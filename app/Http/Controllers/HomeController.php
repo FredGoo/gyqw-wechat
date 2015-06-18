9b8d6f6837ada869f0c506339dbdf90da1d8e52a
FredGoo <545976148@qq.com>
2015-06-18 10:59:26 +0800

    submit apply

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,6 +27,23 @@ class HomeController extends Controller {
    *
    */
   public function submitApplyZan(){
-    var_dump($_POST);
+    $title = \Request::input('title'); // 赞的标题
+    $num = \Request::input('num'); // 赞的数量
+    $content = \Request::input('content'); // 赞的内容
+    $date = date('Y-m-d H:i:s');
+
+    if($title && $num && $content){
+      // 插入数据
+      $res = \DB::insert('insert into zan.`order` (title, num, content, from_user, to_user, created_at, updated_at) values ("?", ?, "?", ?, ?, "?", "?")', [$title, $num, $content, 11, 22, $date, $date]);
+
+      \Log::info('insert into order '.var_export($res ,true));
+      if($res){
+        echo 'success';
+      }else{
+        echo 'fail';
+      }
+    }else{
+      echo 'lack of params';
+    }
   }
 }
